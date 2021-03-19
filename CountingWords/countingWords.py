ea9a2f3120a1b48a0a93525a24e9d6520ef72a1b
arnau <none>
2021-03-19 08:51:08 +0100

    countingWords.py --> change reduce to len

--- CountingWords/countingWords.py
+++ CountingWords/countingWords.py
@@ -1,14 +1,13 @@
 import os.path
-from functools import reduce
 
 def read(*args,) -> None:
 	sum = 0
 	for path in args:
 		if not os.path.isfile(path):
-			sum += reduce(lambda accum, _: accum + 1, path.split(), 0)
+			sum += len(path.split())
 		else:
 			with open(path, 'r') as file:
 				for line in file:
-					sum += reduce(lambda accum, _: accum + 1, line.split(), 0)
+					sum += len(line.split())
 	return sum
 
