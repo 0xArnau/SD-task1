03788f4e6b0e21f00f71a7c4bd014b93ad60633a
Sergi Roviralta <sergi.roviralta@estudiants.urv.cat>
2021-03-30 20:05:08 +0200

    server fusionat master: els workers peten a GetServerResponse

--- master.py
+++ master.py
@@ -1,5 +1,9 @@
 from redis import Redis
 from rq import Queue
+import grpc
+from concurrent import futures      #with master this goes out
+import task_pb2_grpc as pb2_grpc
+import task_pb2 as pb2
 
 import time
 
@@ -11,21 +15,41 @@ REDIS_PORT = 6379
 r = Redis(host=REDIS_HOST, port=REDIS_PORT)
 q = Queue(connection=r)
 
-r.flushall
+class TaskService(pb2_grpc.SendTaskServicer):
+    
+    def __init__(self, *args, **kwargs):
+        pass
 
-t0 = time.time()
-jobs = []
+    def GetServerResponse(self, request, context):
+        jobs = []
+        TaskName = request.task
+        FileName = request.file
 
-for _ in range(10):
- jobs.append(q.enqueue(wordCount, 'test_word_count.txt', result_ttl=-1))			#restult = -1 time where task is in Redis queue
- jobs.append(q.enqueue(countingWords, 'test_word_count.txt', result_ttl=-1))
+        #els workers peten per aqui
 
-#time.sleep(5)
-while any(not job.is_finished for job in jobs):
-	time.sleep(1)
+        jobs.append(q.enqueue(TaskName, FileName, result_ttl=-1))
 
-for job in jobs:
-	print(job.result)
+        while any(not job.is_finished for job in jobs):
+            time.sleep(1)
+        
+        for job in jobs:
+	        print(job.result)
+        
+        result = {'result': jobs[0].result}
+        print(result)
+        
+        return pb2.TaskResponse(**result)
 
-t1 = time.time()
-print(t1 - t0)
+def serve():
+    print("Initialized! We are ready")
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    pb2_grpc.add_SendTaskServicer_to_server(TaskService(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    print("Initializing server...")
+    serve()
+
