3ef01f0f4d4b5d73925c585cbd0e4a1ea516e519
arnau <none>
2021-04-16 11:12:39 +0200

    countingWords -> Now read from a url(http) and not from a file

--- tasks/countingWords.py
+++ tasks/countingWords.py
@@ -1,13 +1,24 @@
 import os.path
+import time
+from urllib.request import urlopen
+import urllib
 
 def countingWords(*args) -> int:
 	sum = 0
-	for path in args:
-		if not os.path.isfile(path):
-			sum += len(path.split())
-		else:
-			with open(path, 'r') as file:
-				for line in file:
-					sum += len(line.split())
+	for link in args:
+		try:
+			sum += len(urlopen(link)
+				.read()
+				.decode('UTF-8')
+				.split()
+			)
+		except urllib.error.HTTPError:
+			sum += len(link
+				.split('http://localhost:8000/')[1]
+				.split()
+			)
 	return sum
 
+if __name__ == "__main__":
+	assert countingWords('http://localhost:8000/test_word_count.txt') == 4
+	assert countingWords('http://localhost:8000/test_word_count.tx') == 1
